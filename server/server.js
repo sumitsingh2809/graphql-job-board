783f2dde25122bb6f3f8f50f1381b2c93baec364
Sumit Singh <sumitsingh@chingari.io>
2022-07-01 15:24:48 +0530

    apollo server setup

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
+import { ApolloServer } from 'apollo-server-express';
+import { readFile } from 'fs/promises';
 import cors from 'cors';
 import express from 'express';
 import { expressjwt } from 'express-jwt';
 import jwt from 'jsonwebtoken';
 import { User } from './db.js';
+import { resolvers } from './resolvers.js';
 
 const PORT = 9000;
 const JWT_SECRET = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
@@ -29,6 +32,12 @@ app.post('/login', async (req, res) => {
     }
 });
 
+const typeDefs = await readFile('./schema.graphql', 'utf8');
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+await apolloServer.start();
+apolloServer.applyMiddleware({ app, path: '/graphql' });
+
 app.listen({ port: PORT }, () => {
     console.log(`Server running on port ${PORT}`);
+    console.log(`Graphql Endpoint: http://localhost:${PORT}/graphql`);
 });
