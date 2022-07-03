bb70817464ff53bc4d41fe1ad081dbee41be0359
Sumit Singh <sumitsingh@chingari.io>
2022-07-03 13:06:19 +0530

    Company Query

--- server/resolvers.js
+++ server/resolvers.js
@@ -2,6 +2,7 @@ import { Job, Company } from './db.js';
 
 export const resolvers = {
     Query: {
+        company: (_root, { id }) => Company.findById(id),
         job: (_root, { id }) => Job.findById(id),
         jobs: () => Job.findAll(),
     },
